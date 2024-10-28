cfd5d2087bc843b8c7b676f7c29a9ad9148de210
Yousef Salah <yousefsalah1242003@gmail.com>
2024-10-29 02:47:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import dotev from "dotenv"
+import { connectDB } from './DB/connection.js'
+import partRouter from './src/modules/part/part.routes.js'
+import authRouter from './src/modules/auth/auth.routes.js'
+
+dotev.config()
+const app = express()
+const port = process.env.PORT
+//parse 
+app.use(express.json())
+// db connection
+await connectDB()
+// routers
+app.use('/part',partRouter)
+app.use('/auth',authRouter)
+// page not found hanle
+app.all('*',(req,res,next)=>{
+    return next( new Error("page not Found",{cause:404}))
+})
+
+// global error handler
+app.use((error,req,res,next)=>{
+    const statusCode = error.cause || 500
+    return res.status(statusCode).json({
+        sucess : false ,
+        message : error.message,
+        stack: error.stack
+
+    })
+})
+
+app.listen(port, () => console.log(` App listening on port ${port}!`))
