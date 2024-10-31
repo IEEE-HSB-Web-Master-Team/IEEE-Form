9d154c66997eab4591d68fcb9c684583875ec15c
Yousef Salah <yousefsalah1242003@gmail.com>
2024-10-31 21:36:38 +0300

    Fix Check Register Logic

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -20,7 +20,6 @@ export async function sendEmail ({to,subject,html } )
         subject,
         html
     } )
-    console.log(info);
     if ( info.accepted.length > 0 ) return true 
     return false
 }
